4dd74a8ed4c635ebec62101a9b8280d6d3887b16
imry <rodkin.imry@gmail.com>
2025-01-13 17:58:30 +0200

    I6

--- include/registries/PropertyRegistry.hpp
+++ include/registries/PropertyRegistry.hpp
@@ -1,31 +1,51 @@
 #pragma once
 #include <vector>
-
 #include "Registry.hpp"
 #include "../../src/model/squares/Property.hpp"
 
 namespace monopoly {
     class PropertyRegistry : public Registry<Property, PropertyID> {
     private:
+        // Ownership tracking (existing)
         std::unordered_map<PlayerID, std::vector<PropertyID>, PlayerID::Hash> ownershipMap;
         std::unordered_map<PropertyID, PlayerID, PropertyID::Hash> propertyOwners;
 
-        void validateProperty(PropertyID propertyId) const;
+        // Color group tracking
+        std::unordered_map<PropertyID, ColorGroupID, PropertyID::Hash> propertyToGroup;
+        std::unordered_map<ColorGroupID, std::vector<PropertyID>, ColorGroupID::Hash> groupProperties;
 
+        // Building tracking
+        std::unordered_map<PropertyID, int, PropertyID::Hash> houseCount;  // 0-4 houses, 5=hotel
+
+        void validateProperty(PropertyID propertyId) const;
         void validateOwnership(PropertyID propertyId) const;
+      //  void validateColorGroup(ColorGroupID groupId) const;
 
     public:
+        // Existing ownership methods
         void setOwner(PropertyID propertyId, PlayerID playerId);
-
         void removeOwner(PropertyID propertyId);
-
         PlayerID getOwner(PropertyID propertyId) const;
-
         std::vector<PropertyID> getProperties(PlayerID playerId) const;
-
         bool hasOwner(PropertyID propertyId) const;
 
-        // Override base class remove to handle ownership cleanup
+        // Color group methods
+        void assignToColorGroup(PropertyID propertyId, ColorGroupID groupId);
+        bool isGroupComplete(ColorGroupID groupId, PlayerID playerId) const;
+        std::vector<PropertyID> getPropertiesInGroup(ColorGroupID groupId) const;
+        ColorGroupID getPropertyGroup(PropertyID propertyId) const;
+
+        // Building methods
+        bool canBuildHouse(PropertyID propertyId) const;
+        void addHouse(PropertyID propertyId);
+        bool upgradeToHotel(PropertyID propertyId);
+        int getHouseCount(PropertyID propertyId) const;
+        bool hasHotel(PropertyID propertyId) const;
+
+        // Override base class remove to handle all cleanup
         bool remove(PropertyID propertyId) override;
+
+        // Rent calculation
+        int calculateRent(PropertyID propertyId) const;
     };
 } // namespace monopoly
--- src/registries/PropertyRegistry.cpp
+++ src/registries/PropertyRegistry.cpp
@@ -8,10 +8,12 @@ namespace monopoly {
     }
 
     void PropertyRegistry::validateOwnership(PropertyID propertyId) const {
+        validateProperty(propertyId);
         if (!hasOwner(propertyId)) {
             throw std::runtime_error("Property has no owner");
         }
     }
+
 
     void PropertyRegistry::setOwner(PropertyID propertyId, PlayerID playerId) {
         validateProperty(propertyId);
@@ -34,11 +36,12 @@ namespace monopoly {
     }
 
     PlayerID PropertyRegistry::getOwner(PropertyID propertyId) const {
-        validateProperty(propertyId);
+        validateOwnership(propertyId);
         auto it = propertyOwners.find(propertyId);
+        /*
         if (it == propertyOwners.end()) {
             throw std::runtime_error("Property has no owner");
-        }
+        }*/
         return it->second;
     }
 
@@ -57,5 +60,102 @@ namespace monopoly {
             return true;
         }
         return false;
+    }
+    void PropertyRegistry::assignToColorGroup(PropertyID propertyId, ColorGroupID groupId) {
+        validateProperty(propertyId);
+
+        // Remove from old group if exists
+        auto oldGroupIt = propertyToGroup.find(propertyId);
+        if (oldGroupIt != propertyToGroup.end()) {
+            ColorGroupID oldGroupId = oldGroupIt->second;
+            auto& properties = groupProperties[oldGroupId];
+            properties.erase(
+                std::remove(properties.begin(), properties.end(), propertyId),
+                properties.end()
+            );
+            if (properties.empty()) {
+                groupProperties.erase(oldGroupId);
+            }
+        }
+
+        // Assign to new group
+        propertyToGroup[propertyId] = groupId;
+        groupProperties[groupId].push_back(propertyId);
+    }
+
+    bool PropertyRegistry::isGroupComplete(ColorGroupID groupId, PlayerID playerId) const {
+        auto it = groupProperties.find(groupId);
+        if (it == groupProperties.end()) {
+            return false;  // Group doesn't exist
+        }
+
+        const auto& properties = it->second;
+        if (properties.empty()) {
+            return false;
+        }
+
+        // Check if all properties in the group belong to the player
+        return std::all_of(properties.begin(), properties.end(),
+            [this, playerId](const PropertyID& propId) {
+                return hasOwner(propId) && getOwner(propId) == playerId;
+            });
+    }
+
+    std::vector<PropertyID> PropertyRegistry::getPropertiesInGroup(ColorGroupID groupId) const {
+        auto it = groupProperties.find(groupId);
+        return it != groupProperties.end() ? it->second : std::vector<PropertyID>{};
+    }
+
+    ColorGroupID PropertyRegistry::getPropertyGroup(PropertyID propertyId) const {
+        validateProperty(propertyId);
+
+        auto it = propertyToGroup.find(propertyId);
+        if (it == propertyToGroup.end()) {
+            throw std::runtime_error("Property is not assigned to any color group");
+        }
+        return it->second;
+    }
+
+    bool PropertyRegistry::canBuildHouse(PropertyID propertyId) const {
+        validateProperty(propertyId);
+
+        // Get current building count
+        auto countIt = houseCount.find(propertyId);
+        int currentHouses = countIt != houseCount.end() ? countIt->second : 0;
+
+        // Can't build more than 4 houses
+        if (currentHouses >= 4) {
+            return false;
+        }
+
+        // Must own all properties in color group
+        ColorGroupID groupId = getPropertyGroup(propertyId);
+        PlayerID owner = getOwner(propertyId);
+        if (!isGroupComplete(groupId, owner)) {
+            return false;
+        }
+
+        // Must maintain even building across color group
+        // All properties in group must have equal or one less house
+        const auto& groupProperties = getPropertiesInGroup(groupId);
+        return std::all_of(groupProperties.begin(), groupProperties.end(),
+            [this, currentHouses, propertyId](const PropertyID& otherId) {
+                if (otherId == propertyId) return true;
+                auto it = houseCount.find(otherId);
+                int otherHouses = it != houseCount.end() ? it->second : 0;
+                return otherHouses >= (currentHouses - 1) && otherHouses <= currentHouses;
+            });
+    }
+
+    void PropertyRegistry::addHouse(PropertyID propertyId) {
+    }
+
+    bool PropertyRegistry::upgradeToHotel(PropertyID propertyId) {
+    }
+
+    int PropertyRegistry::getHouseCount(PropertyID propertyId) const {
+    }
+
+    bool PropertyRegistry::hasHotel(PropertyID propertyId) const {
     }
 } // namespace monopoly
